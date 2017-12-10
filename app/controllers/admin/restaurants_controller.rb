ab50edf24840cfcb31da31da17f813c30eda770b
cjluo <cjluo@cjluode-Air.Home>
2017-12-10 13:01:25 +0800

    create and new restaurant

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -4,6 +4,31 @@ class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_admin
 
   def index
+    @restaurants = Restaurant.all
+
   end
+
+  def new 
+    @restaurant = Restaurant.new
+
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "sucessfully created"
+      redirect_to admin_restaurants_path
+    else
+      flash.now[:alert] = "failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hours, :tel, :address , :description)
+  end
+
 
 end
